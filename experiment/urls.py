81541c83d729aed87d10c2dddd3840d907bf1e9a
Rahul Patidar <ra.patidar12@gmail.com>
2023-10-07 14:24:12 +0100

    added forgot password support

--- experiment/urls.py
+++ experiment/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -11,6 +12,12 @@ urlpatterns = [
     path('sign-in/', views.signInView, name='sign-in'),
     path('sign-up/', views.signUpView, name='sign-up'),
     path('sign-out/', views.signOutView, name='sign-out'),
+
+
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name='password-reset.html'), name='reset_password'),
+    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(template_name='password-reset-done.html') , name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password-reset-confirm.html'), name='password_reset_confirm'),
+    path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view( template_name='password-reset-complete.html') , name='password_reset_complete'),
 
 
 ]
--- redmap/settings.py
+++ redmap/settings.py
@@ -172,3 +172,13 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = os.environ['EMAIL_PORT']
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
