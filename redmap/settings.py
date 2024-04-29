52ed4586a50e48feec036bf7a677d7768245b8c4
Rahul Patidar <ra.patidar12@gmail.com>
2024-04-29 12:07:34 +0100

    removed env variable for config

--- redmap/settings.py
+++ redmap/settings.py
@@ -13,13 +13,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import json
-CONFIG_PATH = os.getenv('CONFIG_PATH') #this is done becasue when launching appache its always best to provide absolute path
-with open(CONFIG_PATH) as config_file: 
-    config = json.load(config_file) 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CONFIG_PATH = os.path.join(BASE_DIR, 'config.json') #this is done becasue when launching appache its always best to provide absolute path
+with open(CONFIG_PATH) as config_file: 
+    config = json.load(config_file) 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
